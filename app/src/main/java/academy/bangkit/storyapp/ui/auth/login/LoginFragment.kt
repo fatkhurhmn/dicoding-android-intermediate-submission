e05d006dd930ecdae0a2a234a6ec4520ba81f8c9
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-13 10:54:22 +0700

    create navigation between LoginFragment.kt and RegisterFragment.kt

--- app/src/main/java/academy/bangkit/storyapp/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/academy/bangkit/storyapp/ui/auth/login/LoginFragment.kt
@@ -6,15 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import academy.bangkit.storyapp.R
+import academy.bangkit.storyapp.databinding.FragmentLoginBinding
+import academy.bangkit.storyapp.ui.auth.register.RegisterFragment
+import androidx.fragment.app.commit
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        moveToRegister()
+    }
+
+    private fun moveToRegister() {
+        binding?.btnToRegister?.setOnClickListener {
+            val mRegisterFragment = RegisterFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.commit {
+                addToBackStack(null)
+                replace(
+                    R.id.auth_container,
+                    mRegisterFragment,
+                    RegisterFragment::class.java.simpleName
+                )
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/academy/bangkit/storyapp/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/academy/bangkit/storyapp/ui/auth/register/RegisterFragment.kt
@@ -1,20 +1,40 @@
 package academy.bangkit.storyapp.ui.auth.register
 
+import academy.bangkit.storyapp.databinding.FragmentRegisterBinding
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import academy.bangkit.storyapp.R
+import androidx.fragment.app.Fragment
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        moveToLogin()
+    }
+
+    private fun moveToLogin() {
+        binding?.btnToLogin?.setOnClickListener {
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.popBackStack()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
