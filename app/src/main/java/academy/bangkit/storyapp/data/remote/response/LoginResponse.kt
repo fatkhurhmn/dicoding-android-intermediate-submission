f72f2b82ac5d228f577bfb58110a66393ea59795
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-14 11:07:14 +0700

    create response class

---
+++ app/src/main/java/academy/bangkit/storyapp/data/remote/response/LoginResponse.kt
@@ -0,0 +1,14 @@
+package academy.bangkit.storyapp.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("loginResult")
+    val loginResult: LoginResult
+)
