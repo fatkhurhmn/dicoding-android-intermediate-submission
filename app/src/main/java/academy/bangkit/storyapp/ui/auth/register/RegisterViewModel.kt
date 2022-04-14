08991c1cf3c0281bb0976cebeb36bd176c1153ab
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-14 11:16:07 +0700

    add function createAccount

--- app/src/main/java/academy/bangkit/storyapp/ui/auth/register/RegisterViewModel.kt
+++ app/src/main/java/academy/bangkit/storyapp/ui/auth/register/RegisterViewModel.kt
@@ -4,5 +4,6 @@ import academy.bangkit.storyapp.data.StoryRepository
 import androidx.lifecycle.ViewModel
 
 class RegisterViewModel(private val storyRepository: StoryRepository) : ViewModel() {
-
+    fun createAccount(name: String, email: String, password: String) =
+        storyRepository.createAccount(name, email, password)
 }
