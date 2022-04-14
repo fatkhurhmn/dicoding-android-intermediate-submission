f3ee0a1d8e404e7bfb05f6dfca7e17fcd61f9229
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-14 11:13:28 +0700

    create initialize view model in authentication

---
+++ app/src/main/java/academy/bangkit/storyapp/ui/auth/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package academy.bangkit.storyapp.ui.auth.login
+
+import academy.bangkit.storyapp.data.StoryRepository
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(storyRepository: StoryRepository) : ViewModel() {
+}
---
+++ app/src/main/java/academy/bangkit/storyapp/ui/auth/register/RegisterViewModel.kt
@@ -0,0 +1,8 @@
+package academy.bangkit.storyapp.ui.auth.register
+
+import academy.bangkit.storyapp.data.StoryRepository
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel(private val storyRepository: StoryRepository) : ViewModel() {
+
+}
