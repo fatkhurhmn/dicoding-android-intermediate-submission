16b59c3c1394ee56febad6c9ef3eddd491536e4a
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-15 14:12:11 +0700

    initialize create story

---
+++ app/src/main/java/academy/bangkit/storyapp/ui/create/CreateStoryActivity.kt
@@ -0,0 +1,52 @@
+package academy.bangkit.storyapp.ui.create
+
+import academy.bangkit.storyapp.R
+import academy.bangkit.storyapp.databinding.ActivityAddStoryBinding
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class CreateStoryActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
+    private lateinit var binding: ActivityAddStoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        binding.toolbarCreate.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.btn_close_create -> {
+                showCloseDialog()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun showCloseDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this).apply {
+            setTitle(getString(R.string.logout))
+            setMessage(getString(R.string.message_close))
+            setCancelable(false)
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                finish()
+            }
+            setNegativeButton(getString(R.string.no)) { dialog, _ ->
+                dialog.cancel()
+            }
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+}
