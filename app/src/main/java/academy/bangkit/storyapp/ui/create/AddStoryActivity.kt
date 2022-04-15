8f10674c49e750f13eff98e127009abe267604fb
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-15 13:53:04 +0700

    add activity to create story

---
+++ app/src/main/java/academy/bangkit/storyapp/ui/create/AddStoryActivity.kt
@@ -0,0 +1,12 @@
+package academy.bangkit.storyapp.ui.create
+
+import academy.bangkit.storyapp.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
