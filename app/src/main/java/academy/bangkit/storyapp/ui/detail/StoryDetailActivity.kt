f8b3eda6b33a088c3e41b824b6dcdff1e4d43a88
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-16 06:55:18 +0700

    implement detail content

--- app/src/main/java/academy/bangkit/storyapp/ui/detail/StoryDetailActivity.kt
+++ app/src/main/java/academy/bangkit/storyapp/ui/detail/StoryDetailActivity.kt
@@ -2,20 +2,45 @@ package academy.bangkit.storyapp.ui.detail
 
 import academy.bangkit.storyapp.R
 import academy.bangkit.storyapp.data.remote.response.Story
-import androidx.appcompat.app.AppCompatActivity
+import academy.bangkit.storyapp.databinding.ActivityStoryDetailBinding
+import academy.bangkit.storyapp.utils.Extension.loadImage
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class StoryDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStoryDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_story_detail)
+        binding = ActivityStoryDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val detail = intent.getParcelableExtra<Story>(EXTRA_DETAIL)
-        Log.d("Detail", "onCreate: $detail")
+        setupContent()
     }
 
-    companion object{
+    private fun setupContent() {
+        val detail = intent.getParcelableExtra<Story>(EXTRA_DETAIL)
+
+        with(binding) {
+            if (detail != null) {
+                imgDetailPhoto.loadImage(detail.photoUrl)
+                tvDetailName.text = detail.name
+                tvDetailDate.text = dateConverter(detail.createdAt)
+                tvDetailDesc.text = getString(R.string.description, detail.description)
+            }
+        }
+    }
+
+    private fun dateConverter(date: String): String {
+        val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+        val formatter = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale.getDefault())
+        return formatter.format(parser.parse(date) as Date)
+    }
+
+    companion object {
         const val EXTRA_DETAIL = "academy.bangkit.storyapp.EXTRA_DETAIL"
     }
 }
