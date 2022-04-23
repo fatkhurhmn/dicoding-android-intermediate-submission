d240db13e24672b407b806791d4ad9fa49ce50a8
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-23 14:08:55 +0700

    add properties data class

--- app/src/main/java/academy/bangkit/storyapp/data/remote/response/StoryResponse.kt
+++ app/src/main/java/academy/bangkit/storyapp/data/remote/response/StoryResponse.kt
@@ -8,12 +8,22 @@ import kotlinx.parcelize.Parcelize
 data class StoryResponse(
     @field:SerializedName("id")
     val id: String,
+
     @field:SerializedName("name")
     val name: String,
+
     @field:SerializedName("description")
     val description: String,
+
     @field:SerializedName("photoUrl")
     val photoUrl: String,
+
     @field:SerializedName("createdAt")
     val createdAt: String,
+
+    @field:SerializedName("lat")
+    val lat: Double? = null,
+
+    @field:SerializedName("lon")
+    val lon: Double? = null,
 ) : Parcelable
