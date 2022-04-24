4c207b831d975e1cc54ac585bcd961183f3189d0
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-04-24 16:59:39 +0700

    implement story with map

--- app/src/main/java/academy/bangkit/storyapp/ui/main/maps/MapsFragment.kt
+++ app/src/main/java/academy/bangkit/storyapp/ui/main/maps/MapsFragment.kt
@@ -1,36 +1,162 @@
 package academy.bangkit.storyapp.ui.main.maps
 
 import academy.bangkit.storyapp.R
+import academy.bangkit.storyapp.data.Result
+import academy.bangkit.storyapp.data.remote.response.StoryResponse
+import academy.bangkit.storyapp.databinding.FragmentMapsBinding
+import academy.bangkit.storyapp.ui.main.MainActivity
+import academy.bangkit.storyapp.utils.Extension.showMessage
+import academy.bangkit.storyapp.utils.ViewModelFactory
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.net.URL
 
 class MapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private lateinit var mMap: GoogleMap
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val mapsViewModel: MapsViewModel by viewModels {
+        ViewModelFactory.getInstance(
+            requireContext()
+        )
     }
+
+    private val callback = OnMapReadyCallback { mMap ->
+        this.mMap = mMap
+        with(mMap.uiSettings) {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        getMyLocation()
+        getAllStory()
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    showStartMarker(location)
+                } else {
+                    binding?.root?.let { view ->
+                        getString(R.string.location_not_found).showMessage(
+                            view
+                        )
+                    }
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun showStartMarker(location: Location) {
+        val startLocation = LatLng(location.latitude, location.longitude)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(startLocation)
+                .title(getString(R.string.my_location))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 12f))
+    }
+
+    private fun getAllStory() {
+        val token = (activity as MainActivity).getToken()
+        if (token != null) {
+            mapsViewModel.getAllStoryWithLocation("Bearer $token")
+                .observe(viewLifecycleOwner) { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding?.progressBarMaps?.visibility = View.VISIBLE
+                        }
+
+                        is Result.Success -> {
+                            binding?.progressBarMaps?.visibility = View.GONE
+                            showAllStory(result.data.stories)
+                        }
+
+                        is Result.Error -> {
+                            binding?.apply {
+                                progressBarMaps.visibility = View.GONE
+                                imgMapsError.visibility = View.VISIBLE
+                                map.visibility = View.GONE
+                            }
+                            binding?.root?.let { view -> result.error.showMessage(view) }
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun showAllStory(stories: List<StoryResponse>) {
+        for (data in stories) {
+            if (data.lat != null && data.lon != null) {
+                val location = LatLng(data.lat, data.lon)
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(location)
+                        .title("Story by ${data.name}")
+                        .snippet(data.description)
+                )
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
